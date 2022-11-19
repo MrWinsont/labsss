4ff1c902f21e1c3ab637cd6b2e049f96574b0ccc
MrWinsont <118475227+MrWinsont@users.noreply.github.com>
2022-11-19 20:04:07 +0200

    new lab6

---
+++ lab6.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include "Funk.hpp"
+using namespace std;
+int main()
+{
+    int mat[100][100];
+    int str, sto;
+    cout << "enter the number of rows and columns"<<endl;
+    cin >> str >> sto;
+    vg::newmat(mat, str, sto);//создание матрицы
+    vg::vivod(mat, str, sto);//вывод введенной матрицы
+    cout << endl;
+    if (vg:: sumdig(vg::max(mat, str, sto)) == vg::sumdig(vg::min(mat, str, sto))) {//сортировка матрицы
+        vg::swap(mat, str, sto);
+    }
+    vg::vivod(mat, str, sto);//вывод новой матрицы
+}
